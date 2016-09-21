f4ddc12264c3cee71092f9b5754b04da46d1ffd4
joosjuha <joosjuha@greeny.cs.tlu.ee>
2016-09-21 14:43:09 +0300

    homework

--- login.php
+++ login.php
@@ -1,87 +1,120 @@
- <?php
- 
-	/*var_dump($_GET);
+<?php 
+	//var_dump($_GET);
 	
-	echo "<br>";
+	//echo "<br>";
 	
-	var_dump($_POST);
-	*/
+	//var_dump($_POST);
+	
 	$signupEmailError = "*";
 	
-	// kas keegi vajutas nuppu ja see on olemas
+	//kas keegi vajutas nuppu ja see on olemas
 	
 	if (isset ($_POST["signupEmail"])) {
-		
-			//on olemas
-			//kas epost on tühi
-			if (empty ($_POST["signupEmail"])){
-				
-				//on tühi
-				$signupEmailError = "Väli on kohustuslik!";
-			}
+		//on olemas
+		// kas epost on tühi
+		if (empty ($_POST["signupEmail"])) {
+			// on tühi
+			$signupEmailError = "* Väli on kohustuslik!";
+		}
 	}
 	
-	// kas epost on tühi
-	
- 	$signupPasswordError = "*";
-	
+	$signupPasswordError = "*";
 	if (isset ($_POST["signupPassword"])) {
-		
-			if (empty ($_POST["signupPassword"])){
-				
-				$signupUsernamePassword = "Väli on kohustuslik!";
-			} else {
-				
-				// parool ei olnud tühi
-				
-				if (strlen($_POST[signupPassword"]) < 8 ) {
-					
-					$signupPasswordError = "* Parool peab olema vähemalt 8 tähemärki pikk"
-				}
+		if (empty ($_POST["signupPassword"])) {
+			$signupPasswordError = "* Väli on kohustuslik!";
+		} else {
+			// parool ei olnud tühi
+			if ( strlen($_POST["signupPassword"]) < 8 ) {
+				$signupPasswordError = "* Parool peab olema vähemalt 8 tähemärkki pikk!";
+			}
+		}
 	}
- 
- ?>
- <!DOCTYPE html>
+	
+	$loginError = "*";
+	if (isset ($_POST["loginEmail"])) {
+		if (empty ($_POST["loginEmail"])) {
+			$loginError = "* Väli on kohustuslik!";
+		}
+	}
+	
+	$loginPasswordError = "*";
+	if (isset ($_POST["loginPassword"])) {
+		if (empty ($_POST["loginPassword"])) {
+			$loginPasswordError = "* Väli on kohustuslik!";
+		}
+	}
+	
+	$homeworkError = "*";
+	if (isset ($_POST["homeworkEmail"])) {
+		if (empty ($_POST["homeworkEmail"])) {
+			$loginError = "* Looks like you forgot your homework email!";
+		}
+	}
+	
+	$homeworkPasswordError = "*";
+	if (isset ($_POST["homeworkPassword"])) {
+		if (empty ($_POST["homeworkPassword"])) {
+			$loginPasswordError = "* You need to enter a password too, doofus.";
+		}
+	}
+	
+	
+	
+?>
+<!DOCTYPE html>
 <html>
 	<head>
-		<title>Login page</title>
+		<title>Sisselogimise leht</title>
 	</head>
 	<body>
 
-		<h1>Login</h1>
+		<h1>Logi sisse</h1>
+		
+		<form method="POST" >
+			
+			<input name="loginEmail" placeholder="E-post" type="email"> <?php echo $loginError; ?>
+			
+			<br><br>
 
-	<form method = "POST">
-		<form>
-			<label>E-mail</label><br>
-			<input name="Email" type = "email">
-		
-			<br><br>
-		
-			<input name="loginPassword" placeholder = "Password" type = "password">
+			<input name="loginPassword" placeholder="Parool" type="password"> <?php echo $loginPasswordError; ?>
 			
 			<br><br>
 			
-			<input type = "submit" value = "Harambe">
- 
+			<input type="submit" value="Logi sisse">
+		
 		</form>
 		
-				<h1>Register</h1>
+		<h1>Loo kasutaja</h1>
+		
+		<form method="POST" >
+			
+			<input name="signupEmail" placeholder = "E-post" type="email"> <?php echo $signupEmailError; ?>
+			
+			<br><br>
 
-	<form method = "POST">
-		<form>
-			<label>E-mail</label><br>
-			<input name="signupEmail" type = "email"> <?php $signupEmailError; ?>
-		
-			<br><br>
-		
-			<input name="signupPassword" placeholder = "Password" type = "password"> <?php $signupPasswordError; ?>
+			<input name="signupPassword" placeholder="Parool" type="password"> <?php echo $signupPasswordError; ?>
 			
 			<br><br>
 			
-			<input type = "submit" value = "Create Harambe">
- 
+			<input type="submit" value="Loo kasutaja">
+		
 		</form>
+
 		
+		<h1>Homework Field</h1>
 		
+		<form method="POST" >
+			
+			<input name="homeworkEmail" placeholder = "Homework Name" type="email"> <?php echo $homeworkError; ?>
+			
+			<br><br>
+
+			<input name="homeworkPassword" placeholder="Homework Password" type="password"> <?php echo $homeworkPasswordError; ?>
+			
+			<br><br>
+			
+			<input type="submit" value="Loo kasutaja">
+		
+		</form>
 	</body>
-</html> 
+</html>
